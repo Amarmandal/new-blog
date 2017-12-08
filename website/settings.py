4e55c2005b5326140bdaad837c4042d3fb5aa27e
Amarmandal <amarmandal2073@gmail.com>
2017-12-08 10:26:21 +0545

    Changed the setting

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2!1^kp4$o%g!nyy2i7dim4^u(h6vx@_z$ifaq8jyhvspk6w4od'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pynepal.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
